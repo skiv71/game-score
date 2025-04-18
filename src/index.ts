7e99343b8f88cbd5f294332cd5dfe8233b59a307
Neil Duffy <skivy71@gmail.com>
2025-04-18 17:21:59 +0100

    testing mongodb.7

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ import { getUsers } from "./db"
 const app = express()
 
 app.get(`/`, async (req: Request, res: Response) => {
-    const users = await getUsers().find().toArray()
-    res.send(users)
+    //const users = await getUsers().find().toArray()
+    res.send(`yo`)
 })
 
 app.listen(80)
