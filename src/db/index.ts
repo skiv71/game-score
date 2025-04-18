657ef5a159b6c45e5b39818e14f30592a16d1f42
Neil Duffy <skivy71@gmail.com>
2025-04-18 17:30:11 +0100

    testing mongodb.8

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,18 +6,14 @@ import {
 
 import { User } from "./models"
 
-const {
-    DB_HOST = `localhost`,
-    DB_PORT = 27017,
-    DB_USER = `mongo`,
-    DB_PASS = `mongo`,
-    DB_NAME = `default`
-} = process.env
+const getURL = (): string => {
+    return `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT || 27017}`
+}
 
-const url = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-
-console.log({ url })
-
-const mongoDB = (db: string = DB_NAME): Db => new MongoClient(url).db(db)
+const mongoDB = (db: string = (process.env.DB_NAME || `default`)): Db => {
+    const url = getURL()
+    console.log(url)
+    return new MongoClient(url).db(db)
+}
 
 export const getUsers = (): Collection<User> => mongoDB().collection<User>(`users`)
