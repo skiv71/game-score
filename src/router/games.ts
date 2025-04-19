a8477f3cbe7b98c80c41db76fe5614c8d38ee6e9
Neil Duffy <skivy71@gmail.com>
2025-04-19 21:41:40 +0100

    get/create game

--- src/router/games.ts
+++ src/router/games.ts
@@ -7,32 +7,44 @@ import { getCollection } from "../db"
 
 import { Game } from "../db/models"
 
+import { MESSAGE } from "../config"
+
 export async function getGames(
     req: Request,
     res: Response
 ) {
-    const games = await getCollection<Game>(`games`)
-        .find()
-        .toArray()
-    res.send(games)
+    try {
+        const games = await getCollection<Game>(`games`)
+            .find()
+            .toArray()
+        res.send(games)
+    } catch(e) {
+        console.error(e)
+        res.status(500).send(MESSAGE.SERVER_ERROR)
+    }
 }
 
 export async function createGame(
     req: Request,
     res: Response
 ): Promise<void> {
-    const name: string = req.body.name || ``
-    if (!name) {
-        res.status(400).send(`Invalid game name!`)
-        return
+    try {
+        const name: string = req.body.name || ``
+        if (!name) {
+            res.status(400).send(`Invalid game name!`)
+            return
+        }
+        const games = getCollection<Game>(`games`)
+        if (await games.findOne({ name })) {
+            res.status(409).send(`Game already exists!`)
+            return
+        }
+        const game = new Game(name)
+        await getCollection<Game>(`games`)
+            .insertOne(game)
+        res.send(game)
+    } catch(e) {
+        console.error(e)
+        res.status(500).send(MESSAGE.SERVER_ERROR)
     }
-    const games = getCollection<Game>(`games`)
-    if (await games.findOne({ name })) {
-        res.status(409).send(`Game already exists!`)
-        return
-    }
-    const game = new Game(name)
-    await getCollection<Game>(`games`)
-        .insertOne(game)
-    res.send(game)
 }
