792599c6b8b9ef0a0fea4c039d10ce8a37a306b0
Neil Duffy <skivy71@gmail.com>
2025-04-26 13:36:21 +0100

    add cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 
+import cors from 'cors'
+
 import router from './router'
 
 import { APP } from "../config"
@@ -7,7 +9,9 @@ import { APP } from "../config"
 export default {
     start(): void {
         express()
-            .use(express.json(), router)
+            .use(cors())
+            .use(express.json())
+            .use(router)
             .listen(APP.PORT, () => {
                 console.log(`app ready, port: ${APP.PORT}`)
             })
